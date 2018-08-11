14f4c651a398530dc03300b3ab58a781db10bbf3
Raphael Beltrand <raphael.beltrand@epitech.eu>
2018-08-11 18:34:00 +0200

    layers and anchors

---
+++ Assets/Scripts/Layer.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class LayerRulesAndState
+{
+    // Layers plateforms respawn rules
+    public float timeToRespawn;
+    public float minRespawnSpeed;
+    public float timeToDecreaseSpawnTime;
+    public float decreaseStep;
+
+    // Layer Start State
+    float lastRespawn = 0.0f;
+    float lastUpdate = 0.0f;
+
+    public void UpdateState()
+    {
+        lastUpdate += Time.deltaTime;
+        lastRespawn += Time.deltaTime;
+
+        if (lastUpdate >= timeToDecreaseSpawnTime)
+        {
+            timeToRespawn -= decreaseStep;
+            if (minRespawnSpeed >= timeToRespawn)
+                timeToRespawn = minRespawnSpeed;
+            lastUpdate = 0.0f;
+        }
+    }
+
+    public bool ShouldRespawn()
+    {
+        return lastRespawn >= timeToRespawn;
+    }
+
+    public void AsRespawn()
+    {
+        lastRespawn = 0.0f;
+    }
+}
+
+public class Layer : MonoBehaviour {
+
+    // The Plateforms of the layer
+    public IAbstractPlatform[] plateforms;
+    // Rules
+    public LayerRulesAndState rules;
+    public bool active;
+
+    // Use this for initialization
+    void Start () {
+        plateforms = GetComponentsInChildren<IAbstractPlatform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rules.UpdateState();
+
+        if (active && rules.ShouldRespawn()) {
+            int toBeActivated = Mathf.RoundToInt(Random.Range(0, GetInactivePlateformsNumber()));
+
+            plateforms[toBeActivated].Activate();
+            rules.AsRespawn();
+        }
+	}
+
+    uint GetInactivePlateformsNumber() {
+        uint ret = 0;
+
+        foreach (IAbstractPlatform plat in plateforms)
+            if (!plat.active)
+                ++ret;
+        return ret;
+    }
+    public void Activate() { active = true; }
+    public void Deactivate() { active = false; }
+}
---
+++ Assets/Scripts/Platforms/PlatformVideo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformVideo : IAbstractPlatform
+{
+    public override void Activate() {
+        base.active = true;
+        base.spriteRenderer.enabled = true;
+    }
+    public override void Deactivate() {
+        base.active = false;
+        base.spriteRenderer.enabled = false;
+    }
+}
