a4e1aa66751492d7126f6175e8bd00c1a57af4a1
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-08-12 15:40:09 +0200

    add timer scripts

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+  public Text timerText;
+  protected float startTimer;
+  public bool started = false;
+
+  // Use this for initialization
+  public virtual void Start () {
+  }
+
+  // Update is called once per frame
+  public virtual void Update () {
+    if(started) {
+      float t = Time.time - startTimer;
+
+      string minutes = (((int) t) / 60).ToString();
+      string seconds = (t % 60).ToString("f2");
+
+      timerText.text = minutes + ": " + seconds;
+    }
+  }
+
+  public virtual void StopTimer() {
+    //timerText.color = Color.yellow;
+    started = false;
+  }
+
+  public virtual void StartTimer() {
+    startTimer = Time.time;
+    started = true;
+  }
+}
