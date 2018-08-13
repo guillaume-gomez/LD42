2492c81b17fa9d3738c4c67ab78feea482349881
Raphael Beltrand <raphael.beltrand@epitech.eu>
2018-08-13 02:11:05 +0200

    New brick skin / lvl 1

---
+++ Assets/Scripts/Platforms/PlatformInput.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformInput : IAbstractPlatform
+{
+
+    public override void Activate()
+    {
+        base.active = true;
+        base.spriteRenderer.enabled = true;
+
+        foreach (Collider2D collider in base.colliders)
+            collider.enabled = true;
+    }
+    public override void Deactivate()
+    {
+        base.active = false;
+        base.spriteRenderer.enabled = false;
+
+        foreach (Collider2D collider in base.colliders)
+            collider.enabled = false;
+    }
+}
