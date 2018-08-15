baf66984472cdc83e674867baa135cee67c36526
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-08-15 15:36:00 +0200

    center camera after wining

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -16,7 +16,9 @@ public class CameraController : MonoBehaviour {
     // LateUpdate is called after Update each frame
     void LateUpdate ()
     {
-        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
-        transform.position = player.transform.position + offset;
+        if(!GameManager.instance.isTransiting) {
+            // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+            transform.position = player.transform.position + offset;
+        }
     }
 }
